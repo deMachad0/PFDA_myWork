82b32ab2adc2cd38f515705aee7c85c1da35812f
Andre <84481409+deMachad0@users.noreply.github.com>
2024-10-31 11:35:02 +0000

    Still learning numpy....

--- Topic05-numpy/messing_numpy.py
+++ Topic05-numpy/messing_numpy.py
@@ -59,10 +59,78 @@ print(a)
 b = a[:2, 1:3]
 print(b)
 
-# 2 ways fo accessing the data in the middle row fo the array
+# 2 ways fo accessing the data in the middle row of the array
 row_r1 = a[1, :]
 row_r2 = a[1:2, :]
 row_r3 = a[[1], :]
 print(row_r1, row_r1.shape)
 print(row_r2, row_r2.shape)
 print(row_r3, row_r3.shape)
+
+# making the same distinction when accessing columns
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print(col_r1, col_r1.shape)
+print(col_r2, col_r2.shape)
+
+# create a new array which we will select elements
+a = np.array([[1,2,3], [4,5,6], [7,8,9], [10,11,12]])
+print(a)
+
+# Create an array of indices
+b = np.array([0, 2, 0, 1])
+
+# Select one element from each row of using the indice in b
+print(a[np.arange(4), b])
+print()
+# mutate one element from each row of a using the indice in b
+a[np.arange(4), b] += 10
+print(a)
+
+# boolean array indexing: pick elements of an array to satisfy some condition
+a = np.array([[1,2], [3,4], [5,6]])
+
+bool_idx = (a > 2) # finds an element of a that are bigger than 2
+                   # this returns a boolean
+print(bool_idx)
+
+# we can take the values of the original array and print it 
+print(a[bool_idx])
+# we can also do all the above in a concise way
+print(a[a > 2])
+print()
+
+# some useful functions to be used: argmax (get the maximum element), argmin (get the minimun element)
+# argsort (get sorted list of indices, by element value, in ascending)
+# where (get indices of elements that meet some condition)
+a = np.array([1, 8, 9, -3, 2, 4, 7, 9])
+print(np.argmax(a)) # index maximum
+print(np.argmin(a)) # index minimun
+print(np.argsort(a))
+# get sorted list of indices in descending order
+print(np.argsort(a)[:: -1])
+# this returns a tuple, the list of indices is the first entry, use [0]
+print(np.where(a > 5)[0])
+
+# this example shows how to get te index of all the max values
+print(np.where(a >= a[np.argmax(a)])[0])
+
+# Array math
+# creating an array using random
+a = np.random.random(100000000)
+print(a)
+# seing the time spent to sum all array
+#%%time 
+x = np.sum(a) # takes less time
+# seing the time spent to sum all array using for 
+#%%time 
+x = 0
+for element in a:
+    x = x + element # takes too much time
+
+# adding arrays together
+x = np.array([[1,2], [3,4]])
+y = np.array([[5,6], [7,8]])
+
+print(x + y)
+print(np.add(x, y))
